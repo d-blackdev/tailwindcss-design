af4768bd5be2637cbf976024259c9449bcaf7dbc
Lasisi kehinde <lasisikehinde153@gmail.com>
2020-04-30 14:04:08 +0100

    Finished the project
    Completed the the frontend project

--- build/plugins/index.js
+++ build/plugins/index.js
@@ -3,7 +3,28 @@ new WOW().init();
 
 // owl carousel
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        animateOut: 'slideOutDown',
+        animateIn:'flipInX',
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 2,
+                nav: true,
+                loop: false
+            }
+        }
+    });
 });
 
 // menu nav control
@@ -19,20 +40,51 @@ let showMenu = false;
 menuBtn.addEventListener('click',()=>{
     if (!showMenu){
         hamburger.classList.add('open');
+        // hamburger.classList.add("blue")
         slideList.classList.add('open');
         showMenu = true;
         
     }
     else{
         hamburger.classList.remove('open');
+        // hamburger.classList.remove('blue');
         slideList.classList.remove('open')
         showMenu = false;
     }
 });
         
-       
-        
-        
+// Tab links
+const dropBtn = document.querySelector('.dropdown-btn');
+const dropContainer = document.querySelector('.dropdown-container')
+dropBtn.addEventListener('click', ()=>{
+    if(!showMenu){
+        dropBtn.classList.add('rotate');
+        dropContainer.classList.add('open-dropdown')
+        showMenu = true;
+    }
+    else{
+        dropBtn.classList.remove('rotate');
+        dropContainer.classList.remove('open-dropdown')
+
+        showMenu = false;
+    }
+})
+// support icon function
+const supportIcon = document.querySelector('.support-dropdown-btn');
+const supportContent = document.querySelector('.support-content')
+supportIcon.addEventListener('click', ()=>{
+    if (!showMenu){
+        supportIcon.classList.add('rotate');
+        supportContent.classList.add('open-support');
+        showMenu = true;
+    }
+    else{
+        supportIcon.classList.remove('rotate');
+        supportContent.classList.remove('open-support');
+        showMenu = false;
+    }
+})
+
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,12 @@ module.exports = {
       'minSm': '640px',
       'minMd': '768px',
       'minLg': '1024px',
+      'minXl':  '1440px',
+      'maxXs': {'max':'450px'},
       'maxSm': {'max': '640px'},
       'maxMd': {'max': '768px'},
       'maxLg': {'max': '1024px'},
+      
     },
     extend: {},
   },
